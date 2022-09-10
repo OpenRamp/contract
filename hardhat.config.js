523546443b127ecfd635e0eba84f125c858594b9
Leonardo <leonardo.k2@ciclic.com.br>
2022-09-10 19:00:03 -0300

    Adding the statments for use MATIC Mumbai testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,2 +1,13 @@
-require("@nomiclabs/hardhat-ethers");
 require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+    defaultNetwork: "matic",
+    networks: {
+        matic: {
+            url: "https://rpc-mumbai.maticvigil.com",
+            accounts: [process.env.MATIC_DEPLOYER_PRIVATE_KEY]
+        },
+    },
+    solidity: "0.8.4"
+};
